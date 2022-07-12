a97a77feb8df24199980efcc9d86479cb1c50f73
Brendo Silva <breendo.01@gmail.com>
2022-07-12 10:08:02 -0300

    component button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
+import { NotificationButton } from "./components/NotificationButton"
 
 
 function App() {
 
   return (
-    <h1>Hello mundo</h1>
+    <NotificationButton />
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import avatar from '../../assets/avatar-icon.svg';
+import './styles.css';
+
+export function NotificationButton() {
+  return (
+    
+    <div className="dsmeta-red-btn">
+      <img src={avatar} alt="notificação button" />
+    </div>
+    
+  );    
+}
